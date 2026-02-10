70113ff697b4215f44238dfef658733a3a0fe59e
Atharv Kulakrni <atharvkulkarni979@gmail.com>
2026-02-10 11:33:25 +0530

    Added the count

--- src/App.tsx
+++ src/App.tsx
@@ -71,8 +71,8 @@ const App = () => {
                 />
               </section>
               <section id="register">
-                {/* <RecruitmentSection onOpenRegistration={handleOpenRegistration} /> */}
-                <RegistrationEnd />
+                <RecruitmentSection onOpenRegistration={handleOpenRegistration} />
+                {/* <RegistrationEnd /> */}
               </section>
               <section id="footer">
                 <Footer />
